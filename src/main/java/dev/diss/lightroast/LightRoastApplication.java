61ca91d3fa70787c36e4301b92d030af4cd1a41c
diss <65290988+Dissurender@users.noreply.github.com>
2023-10-17 08:22:01 -0500

    init commit

---
+++ src/main/java/dev/diss/lightroast/LightRoastApplication.java
@@ -0,0 +1,13 @@
+package dev.diss.lightroast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightRoastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightRoastApplication.class, args);
+    }
+
+}
